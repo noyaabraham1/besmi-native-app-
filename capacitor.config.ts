a4b25f1fe021047d7f4a04199fd9f48700870ffc
noyaabraham1 <noya.abraham@gmail.com>
2025-07-01 00:18:32 -0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,37 @@
+### 3. capacitor.config.ts
+
+```typescript
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.besmi.lashbooking',
+  appName: 'Besmi',
+  webDir: '.',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true,
+    webContentsDebuggingEnabled: false
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      backgroundColor: "#000000",
+      showSpinner: false,
+      androidSplashResourceName: "splash",
+      splashFullScreen: true,
+      splashImmersive: true
+    },
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: '#000000'
+    },
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
+};
+
+export default config;
+```
