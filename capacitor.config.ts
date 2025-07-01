91dcf86d71514c4da74b3c1c91617a7647dcb1cf
noyaabraham1 <noya.abraham@gmail.com>
2025-07-01 01:26:22 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,36 +1,29 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.besmi.lashbooking',
-  appName: 'Besmi',
-  webDir: '.',
-  server: {
-    androidScheme: 'https'
+{
+  "appId": "com.besmi.lashbooking",
+  "appName": "Besmi",
+  "webDir": ".",
+  "server": {
+    "androidScheme": "https"
   },
-  android: {
-    allowMixedContent: true,
-    webContentsDebuggingEnabled: false
+  "android": {
+    "allowMixedContent": true,
+    "webContentsDebuggingEnabled": false
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 1000,
-      backgroundColor: "#000000",
-      showSpinner: false,
-      androidSplashResourceName: "splash",
-      splashFullScreen: true,
-      splashImmersive: true
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 1000,
+      "backgroundColor": "#000000",
+      "showSpinner": false,
+      "androidSplashResourceName": "splash",
+      "splashFullScreen": true,
+      "splashImmersive": true
     },
-    StatusBar: {
-      style: 'dark',
-      backgroundColor: '#000000'
+    "StatusBar": {
+      "style": "dark",
+      "backgroundColor": "#000000"
     },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
+    "PushNotifications": {
+      "presentationOptions": ["badge", "sound", "alert"]
     }
   }
-};
-
-export default config;
-
-export default config;
-```
+}
